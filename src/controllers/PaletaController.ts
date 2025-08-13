c063a5aed88e83279fa19f08b595764fd8eebadf
Maria Luiza Fica Borges <marialuizaficaborges@gmail.com>
2025-08-13 17:26:21 -0300

    Implementação inicial funcionalidades de Paleta
    Implementação inicial do repository, controller e routes da classe Paleta

--- src/controllers/PaletaController.ts
+++ src/controllers/PaletaController.ts
@@ -1,7 +1,51 @@
 import { Request, Response } from "express";
+import { PaletaRepository } from "repositories/PaletaRepository";
 import { inject, injectable } from "tsyringe";
 
 @injectable()
 export class PaletaController {
-   
+    constructor(
+        @inject('PecaRepository')
+        private pecaRepository: PaletaRepository
+    ) {}
+
+    async createPaleta(req: Request, res: Response): Promise<Response>{
+        try{
+            return;
+        } catch(error){
+
+        }
+    }
+
+    async deletePaleta(req: Request, res: Response){
+        try{
+            return;
+        } catch(error){
+
+        }
+    }
+
+    async listPaletas(req: Request, res: Response){
+        try{
+            return;
+        } catch(error){
+
+        }
+    }
+
+    async findPaletaById(req: Request, res: Response){
+        try{
+            return;
+        } catch(error){
+
+        }
+    }
+
+    async updatePaleta(req: Request, res: Response){
+        try{
+            return;
+        } catch(error){
+
+        }
+    }
 }
--- src/routes/paletaRoutes.ts
+++ src/routes/paletaRoutes.ts
@@ -1,6 +1,53 @@
 import { Router } from "express";
+import { PaletaController } from "../controllers/PaletaController";
+import { container } from "tsyringe";
 
 const router = Router();
+const paletaController = container.resolve(PaletaController);
 
+// Cria 
+router.post('/', async (req, res, next) => {
+    try {
+        await paletaController.createPaleta(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Deleta
+router.delete('/:id', async (req, res, next) => {
+    try {
+        await paletaController.deletePaleta(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Lista 
+router.get('/', async (req, res, next) => {
+    try {
+        await paletaController.listPaletas(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Busca por ID
+router.get('/:id', async (req, res, next) => {
+    try {
+        await paletaController.findPaletaById(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Atualiza 
+router.put('/:id', async (req, res, next) => {
+    try {
+        await paletaController.updatePaleta(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
 
 export default router;
