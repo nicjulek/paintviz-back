3d337febb2c1d07ffa24c5d962bec7151ad65b3e
Nicole Julek Klazura <nicolejklazura@gmail.com>
2025-08-18 09:57:06 -0300

    Adiciona métodos e atualiza para aplicação de cor

--- src/container/index.ts
+++ src/container/index.ts
@@ -2,9 +2,12 @@ import { container } from "tsyringe";
 import { PinturaRepository } from "../repositories/PinturaRepository";
 import { UsuarioRepository } from "../repositories/UsuarioRepository";
 import { ClienteRepository } from "../repositories/ClienteRepository";
+import { CarroceriaRepository } from "../repositories/CarroceriaRepository";
+import { PecaRepository } from "repositories/PecaRepository";
 
 container.registerSingleton('PinturaRepository', PinturaRepository);
 container.registerSingleton('ClienteRepository', ClienteRepository);
 container.registerSingleton('UsuarioRepository', UsuarioRepository);
+container.registerSingleton('PecaRepository', PecaRepository);
 
 export { container }
