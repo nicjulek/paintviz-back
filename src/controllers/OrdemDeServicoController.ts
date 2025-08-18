f7ed36e310f844c45fd52cfd0aa140e96d7236b8
Maria Luiza Fica Borges <marialuizaficaborges@gmail.com>
2025-08-17 21:21:29 -0300

    Implementação inicial funções básicas OrdemDeServiço
    Implementação inicial das funções básicas no repository, controller e routes da classe OrdemDeServico

--- src/controllers/OrdemDeServicoController.ts
+++ src/controllers/OrdemDeServicoController.ts
@@ -1,7 +1,59 @@
 import { Request, Response } from "express";
+import { OrdemDeServicoRepository } from "repositories/OrdemDeServicoRepository";
 import { inject, injectable } from "tsyringe";
 
 @injectable()
 export class OrdemDeServicoController {
-   
+constructor(
+        @inject('OrdemDeServicoRepository')
+        private ordemDeServicoRepository: OrdemDeServicoRepository
+    ) {}
+
+    async createOrdemDeServico(req: Request, res: Response){
+        try{
+
+        } catch(error){
+
+        }
+    }
+
+    async deleteOrdemDeServico(req: Request, res: Response){
+        try{
+
+        } catch(error){
+            
+        }
+    }
+
+    async listOrdemDeServico(req: Request, res: Response){
+        try{
+
+        } catch(error){
+            
+        }
+    }
+
+    async findById(req: Request, res: Response){
+        try{
+
+        } catch(error){
+            
+        }
+    }
+
+    async findByCliente(req: Request, res: Response){
+        try{
+
+        } catch(error){
+            
+        }
+    }
+
+    async updateOrdemDeServico(req: Request, res: Response){
+        try{
+
+        } catch(error){
+            
+        }
+    }
 }
--- src/routes/ordemServicoRoutes.ts
+++ src/routes/ordemServicoRoutes.ts
@@ -1,6 +1,61 @@
 import { Router } from "express";
+import { OrdemDeServicoController } from "../controllers/OrdemDeServicoController";
+import { container } from "tsyringe";
 
 const router = Router();
+const ordemDeServicoController = container.resolve(OrdemDeServicoController);
 
+// Cria 
+router.post('/', async (req, res, next) => {
+    try {
+        await ordemDeServicoController.createOrdemDeServico(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Deleta
+router.delete('/:id', async (req, res, next) => {
+    try {
+        await ordemDeServicoController.deleteOrdemDeServico(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Lista 
+router.get('/', async (req, res, next) => {
+    try {
+        await ordemDeServicoController.listOrdemDeServico(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Busca por ID
+router.get('/:id', async (req, res, next) => {
+    try {
+        await ordemDeServicoController.findById(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/:id_cliente', async (req, res, next) => {
+    try {
+        await ordemDeServicoController.findByCliente(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Atualiza 
+router.put('/:id', async (req, res, next) => {
+    try {
+        await ordemDeServicoController.updateOrdemDeServico(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
 
 export default router;
