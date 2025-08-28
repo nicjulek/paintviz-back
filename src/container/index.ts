a332b503e39f7ec31cda67b3813b9c58d675218d
Maria Luiza Fica Borges <marialuizaficaborges@gmail.com>
2025-08-28 17:14:35 -0300

    Alteração no container

--- src/container/index.ts
+++ src/container/index.ts
@@ -4,11 +4,13 @@ import { UsuarioRepository } from "../repositories/UsuarioRepository";
 import { ClienteRepository } from "../repositories/ClienteRepository";
 import { OrdemDeServicoRepository } from "../repositories/OrdemDeServicoRepository";
 import { StatusRepository } from "../repositories/StatusRepository";
+import { PecaRepository } from "../repositories/PecaRepository";
 
 container.registerSingleton('PinturaRepository', PinturaRepository);
 container.registerSingleton('ClienteRepository', ClienteRepository);
 container.registerSingleton('UsuarioRepository', UsuarioRepository);
 container.registerSingleton('OrdemDeServicoRepository', OrdemDeServicoRepository);
 container.registerSingleton('StatusRepository', StatusRepository);
+container.registerSingleton('PecaRepository', PecaRepository);
 
 export { container }
